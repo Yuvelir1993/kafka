cb05156cd38e269805ccf02389c8546e7bb77760
plozovik <plozovikov@gmail.com>
2023-10-09 17:58:09 +0200

    websockets

--- js-consumer/app.js
+++ js-consumer/app.js
@@ -1,7 +1,14 @@
+const { createServer } = require('http');
 const { Kafka } = require('kafkajs');
 const kafka = new Kafka({
     clientId: 'js-consumer',
-    brokers: ['172.29.17.18:9094'] 
+    brokers: ['172.29.17.18:9094']
+});
+const httpServer = createServer();
+const io = require('socket.io')(httpServer);
+
+io.on("connection", (socket) => {
+    console.log('WebSocket client connected');
 });
 
 const consumer = kafka.consumer({ groupId: 'test-group' });
@@ -11,12 +18,18 @@ const consume = async () => {
     await consumer.subscribe({ topic: topic, fromBeginning: true });
     await consumer.run({
         eachMessage: async ({ topic, partition, message }) => {
-            console.log({
+            const data = {
                 value: message.value.toString(),
-            });
+            };
+            console.log('Received data from Kafka:', data);
+            io.emit('message', data);
         },
     });
 };
 consume().catch((err) => {
     console.error("error in consumer: ", err)
+});
+
+httpServer.listen(8080, () => {
+    console.log('WebSocket server is running on port 8080.');
 });
--- web/src/App.js
+++ web/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:8080');
 
 function App() {
+  useEffect(() => {
+    socket.on('message', (data) => {
+      // Handle incoming data from Kafka consumer here
+      console.log('Received data from Kafka consumer:', data);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Your React UI */}
     </div>
   );
 }
