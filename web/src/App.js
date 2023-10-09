6ee8109d5b76a882046ae3578a2e14300cb4e7a8
plozovik <plozovikov@gmail.com>
2023-10-09 21:00:51 +0200

    Using 'localStorage' to not loose UI data on refresh

--- web/src/App.js
+++ web/src/App.js
@@ -6,14 +6,22 @@ import TextLines from './TextLines';
 // TODO: use this below, but currently is undefined
 // const socket = io(`http://${process.env.ETH0_IP}:8080`);
 const socket = io(`http://172.29.17.18:8080`);
+const STORAGE_KEY = 'kafkaMessages';
 
 function App() {
-  const [messages, setMessages] = useState([]);
+  const [messages, setMessages] = useState(() => {
+    const storedMessages = localStorage.getItem(STORAGE_KEY);
+    return storedMessages ? JSON.parse(storedMessages) : [];
+  });
   console.log(process.env);
   useEffect(() => {
     socket.on('message', (data) => {
       console.log('Received data from Kafka consumer: ', data);
-      setMessages((prevMessages) => [...prevMessages, data.value]);
+      setMessages((prevMessages) => {
+        const newMessages = [...prevMessages, data.value];
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(newMessages));
+        return newMessages;
+      });
     });
   }, []);
 
